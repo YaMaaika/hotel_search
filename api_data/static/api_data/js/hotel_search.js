014d4ff88a9efa30d4bb1676c2052ef8b4a7155d
Maaike <maaikeb@protonmail.com>
2025-11-30 10:52:41 +0100

    Changed identation in JS file and small change in template
    - Fixed identation in JS file
    - Small change in template: add a line 'Hotels in <city>'
    - In JS file added removing the city from the input box after page load

--- api_data/static/api_data/js/hotel_search.js
+++ api_data/static/api_data/js/hotel_search.js
@@ -1,40 +1,45 @@
-     async function loadCities() {
-            try {
-                const response = await fetch('/hotel_search/get_cities/');
+ async function loadCities() {
+    try {
+        const response = await fetch('/hotel_search/get_cities/');
 
-                if (!response.ok) {
-                    throw new Error('Failed to load cities');
-                }
-
-                allCities = await response.json();
-                console.log(`Loaded ${allCities.length} cities`);
-
-                return allCities;
-
-            } catch (error) {
-                console.error('Error loading cities:', error);
-            }
+        if (!response.ok) {
+            throw new Error('Failed to load cities');
         }
 
-        document.addEventListener('DOMContentLoaded', function() {
-            var allCities = loadCities();
+        allCities = await response.json();
+        console.log(`Loaded ${allCities.length} cities`);
 
-            var config = {
-                placeHolder: "Search for a city to view hotels...",
-                data: {
-                    src: allCities
-                },
-                resultItem: {
-                    highlight: true,
-                },
-                events: {
-                    input: {
-                        selection: (event) => {
-                            const selection = event.detail.selection.value;
-                            autoCompleteJS.input.value = selection;
-                        }
-                    }
+        return allCities;
+
+    } catch (error) {
+        console.error('Error loading cities:', error);
+    }
+}
+
+document.addEventListener('DOMContentLoaded', function() {
+    document.getElementById('autoComplete').value = '';
+
+    var allCities = loadCities();
+
+    var config = {
+        placeHolder: "Search for a city to view hotels...",
+        data: {
+            src: allCities
+        },
+        resultItem: {
+            highlight: true,
+        },
+        events: {
+            input: {
+                selection: (event) => {
+                    const selection = event.detail.selection.value;
+                    autoCompleteJS.input.value = selection;
                 }
             }
-            const autoCompleteJS = new autoComplete(config);
-        });
+        }
+    }
+    const autoCompleteJS = new autoComplete(config);
+});
+
+window.onload = function() {
+};
